d6d6a17de8c96fda51731eea96d15d18c11e5896
Hugo Ye <hugoye122@gmail.com>
2022-04-05 14:42:32 +0300

    homework3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,22 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.io.*;
-import java.util.Scanner;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 10000; i++) {
-            list.add(i);
-        }
-        homework1a(list);
+        homework3();
 
+    }
+
+    public static ArrayList<Integer> list() {
+        ArrayList<Integer> templist = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            templist.add(i);
+        }
+        return templist;
     }
 
     public static void homework1a(List<Integer> list) {
@@ -60,32 +63,30 @@ public class Main {
             System.out.println(e);
         }
     }
-     /*
-    CREATE TABLE movie (
-            id int PRIMARY KEY,
-            name nvarchar(100) NOT NULL,
-    yearOP int NOT NULL,
-    director nvarchar(100) NOT NULL REFERENCES director(id)
-            );
 
-    CREATE TABLE director (
-            id int PRIMARY KEY,
-            firstname nvarchar(100) NOT NULL,
-    lastname nvarchar(100) NOT NULL
-);
-
-    INSERT INTO movie (id, name, yearOP, director) VALUES
-  (1, 'Jurrassic park', 1993, 1),
-          (2, 'Interstellar', 2014, 2),
-          (3, 'Inglorious Bastards', 2009, 3);
-
-
-    INSERT INTO director (id, firstname, lastname) VALUES
-  (1, "Steven", "Spielberg"),
-          (2, "Christopher", "Nolan"),
-          (3, "Quentin", "Tarantino");
-*/
 
     public static void homework3() {
+        ArrayList<Movie> movies = new ArrayList<Movie>();
+        String database = "jdbc:ucanaccess://data/Homework2.accdb";
+        try (Connection connection = DriverManager.getConnection(database)) {
+            String sql = "SELECT movie.title, movie.yearOP, director.firstname, director.lastname FROM movie JOIN director ON movie.director=Director.id";
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+
+            while (result.next()) {
+                String title = result.getString("title");
+                int yearOP = result.getInt("yearOP");
+                String directorFN = result.getString("director.firstname");
+                String directorLN = result.getString("director.lastname");
+
+                movies.add(new Movie(title, yearOP, String.format("%s %s", directorFN, directorLN)));
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        for (Movie movie : movies) {
+            System.out.println(movie.toString());
+        }
     }
 }
